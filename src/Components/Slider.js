8591b30f346a038dd4a507cc2d32312e11afa9bf
anna johnson <110annajay@gmail.com>
2023-05-29 13:17:27 +0100

    propotioning  commit

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -6,7 +6,7 @@ function Slider(props) {
     "https://res.cloudinary.com/dp6afxo4t/image/upload/v1685204038/doviee/female/freestocks-_3Q3tsJ01nc-unsplash_tmyesa.jpg",
     "https://res.cloudinary.com/dp6afxo4t/image/upload/v1685204036/doviee/female/tamara-bellis-IwVRO3TLjLc-unsplash_shbmvy.jpg",
     "https://res.cloudinary.com/dp6afxo4t/image/upload/v1685204033/doviee/female/force-majeure-00tlC0Clfrs-unsplash_n0hjtu.jpg",
-  ]; // Replace with your image URLs
+  ];
   const mimages = [
     "https://res.cloudinary.com/dp6afxo4t/image/upload/v1685204076/doviee/male/andrew-neel-HqtYwlY9dxs-unsplash_ssde6y.jpg",
     "https://res.cloudinary.com/dp6afxo4t/image/upload/v1685204069/doviee/male/hunters-race-hNoSCxPWYII-unsplash_pd6akk.jpg",
