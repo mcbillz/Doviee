8bed046b04b5a0facf21ca6dad002a6ecaf70436
anna johnson <110annajay@gmail.com>
2023-06-07 22:55:51 +0100

    cors

--- Server/index.js
+++ Server/index.js
@@ -4,7 +4,22 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://doviee.vercel.app",
+  })
+);
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"),
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"),
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization"
+    ),
+    res.setHeader("Access-Control-Allow-Credentials", "true"),
+    next();
+});
 
 app.get("/api/Products", (req, res) => {
   res.json(Products);
