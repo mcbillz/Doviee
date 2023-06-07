f5e8ec00f6f485ee9b3df5efbca112e26033be81
anna johnson <110annajay@gmail.com>
2023-06-07 22:59:21 +0100

    cors

--- Server/index.js
+++ Server/index.js
@@ -11,7 +11,7 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*"),
+  res.setHeader("Access-Control-Allow-Origin", "https://doviee.vercel.app"),
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"),
     res.setHeader(
       "Access-Control-Allow-Headers",
